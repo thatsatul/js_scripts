a7d77d09eaa5c83589a3258ec6ab232aa11f01f6
Atul Anand <atul.anand@travenues.com>
2020-07-28 17:40:56 +0530

    deleting folder

--- interviews/dkatalis/src/index.js
+++
@@ -1,5 +0,0 @@
-const filePath = process.argv[2];
-const fileOperations = require('./fileOperations');
-
-const consoleOutput = fileOperations.readFileData(filePath);
-consoleOutput && consoleOutput.split('\n').forEach(val => console.log(val));
